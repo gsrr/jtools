4d6b430fb6561d4425862e0efbfd4913faf12291
root <root@localhost.localdomain>
2019-04-23 03:37:02 -0400

    update

--- ubuntu/build_linux.sh
+++ ubuntu/build_linux.sh
@@ -2,6 +2,10 @@
 
 # Reference
 ## https://jerrynest.io/ubuntu-16-04-compile-linux-kernel/
+apt install bison -y
+apt install flex -y
+apt install libelf-dev -y
+apt-get install build-essential ncurses-dev libssl-dev build-essential ncurses-dev xz-utils kernel-package -y
 
 # Download source code of linux kernel
 wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/linux/4.18.0-10.11/linux_4.18.0.orig.tar.gz
